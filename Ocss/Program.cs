d6a7a57a798e8ef51a382a21ab567e3fc2b7bc3e
gavin <gavinluo@qt-asia.com>
2022-04-27 15:03:54 +0800

    提交Demo，控制台输出，输入，字符串拼接

---
+++ Ocss/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ocss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //输出文字
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("请输入你的姓名：");
+            //读取控制台输入的字符串
+            var yourName = Console.ReadLine();
+            //清除控制台所有内容
+            Console.Clear();
+            //拼接字符串输出到控制台
+            Console.WriteLine($"Your input is {yourName}");
+        }
+    }
+}
