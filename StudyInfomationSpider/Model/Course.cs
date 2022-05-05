25ac3f06e3e4cad3e398b98d1520c0236a50bd30
gavin <gavinluo@qt-asia.com>
2022-05-05 14:47:16 +0800

    提交爬虫Demo

---
+++ StudyInfomationSpider/Model/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyInfomationSpider.Model
+{
+    /// <summary>
+    /// 课程信息
+    /// </summary>
+    internal class Course
+    {
+        public string CourseName { get; set; }
+        public string UserID { set; get; }
+        public int StudyPercent { get; set; }
+        public int TestCodes { set; get; }
+        public int NoteCount { set; get; }
+
+    }
+}
---
+++ StudyInfomationSpider/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using StudyInfomationSpider.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyInfomationSpider.Model
+{
+    /// <summary>
+    /// 学生信息
+    /// </summary>
+    internal class User
+    {
+        public User()
+        {
+            Courses = new List<Course>();
+        }
+        public string Name { get; set; }
+        public string ID { set; get; }
+        public string NickName { set; get; }
+        public Uri MoocUrl { set; get; }
+        public List<Course> Courses { set; get; }
+    }
+
+
+}
